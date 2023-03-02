5c31ad97624228a35e4a3469bd5270fc6b54fee4
MHMK <khanihasan1381@gmail.com>
2023-03-02 12:58:04 +0330

    Complete Login page and logining and some changing.

--- account_module/urls.py
+++ account_module/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('register', views.RegisterUser.as_view(), name='register-page'),
-    path('login', views.LoginUser.as_view(), name='login-page')
+    path('login', views.LoginUser.as_view(), name='login-page'),
+    path('active-account<str:email_activation_code>', views.ActivationUser.as_view())
 ]
--- account_module/views.py
+++ account_module/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import login
+from django.http import HttpRequest, Http404
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.utils.crypto import get_random_string
@@ -23,6 +25,7 @@ class RegisterUser(View):
             password = request.POST['password']
             user = User(username=username, email=email, email_activation_code=get_random_string(72))
             user.set_password(password)
+            user.is_active = False
             user.save()
             # TODO: send activation code
             return redirect('login-page')
@@ -42,5 +45,36 @@ class LoginUser(View):
         }
         return render(request, 'account_module/login-register.html', context)
 
-    def post(self):
-        pass
+    def post(self, request):
+        login_form = LoginForm(request.POST)
+        context = {
+            'login_form': login_form
+        }
+        username = request.POST['username']
+        password = request.POST['password']
+        user: User = User.objects.all().filter(username__exact=username).first()
+        if user is None:
+            login_form.add_error('username', 'نام کاربری و یا کلمه عبور اشتباه است.')
+            return render(request, 'account_module/login-register.html', context)
+        if not user.check_password(password):
+            login_form.add_error('username', 'نام کاربری و یا کلمه عبور اشتباه است.')
+            return render(request, 'account_module/login-register.html', context)
+        if not user.is_active:
+            login_form.add_error('username', 'اکانت شما فعال نشده است.')
+            return render(request, 'account_module/login-register.html', context)
+        login(request, user)
+        return redirect('home')
+
+
+class ActivationUser(View):
+    def get(self, request, email_activation_code):
+        user: User = User.objects.all().filter(email_activation_code__exact=email_activation_code).first()
+        if user is None:
+            raise Http404
+        if user.is_active:
+            # todo: show error message
+            pass
+        else:
+            user.is_active = True
+            user.email_activation_code = get_random_string(72)
+            return redirect('login-page')
